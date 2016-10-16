e511f2a8a255c7dc4c8c741b080d0f4014496701
GayanSilva <gayan.silva@gmail.com>
2016-10-16 16:31:44 +1100

    Added Sales Order Item class

---
+++ SolutionName.Model/SalesOrderItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionName.Model
+{
+    public class SalesOrderItem
+    {
+        public int TotalCount { get; set; }
+    }
+}
